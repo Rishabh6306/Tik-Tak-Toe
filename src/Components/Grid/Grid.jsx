5c913d7457dd598a63558e6901865790d36a60bc
Rishabh Srivastava <rishabhsrivastav722@gmail.com>
2023-08-09 20:13:31 +0530

    Initial Commit

--- src/Components/Grid/Grid.jsx
+++ src/Components/Grid/Grid.jsx
@@ -1,27 +1,68 @@
 import React, { useState } from 'react'
 import Card from '../Card/Card'
 import './Grid.css'
-import { FaLastfm } from 'react-icons/fa'
+
+function isWinner(board, symbol) {
+    if (board[0] === board[1] && board[1] === board[2] && board[2] === symbol) return symbol;
+    if (board[3] === board[4] && board[4] === board[5] && board[5] === symbol) return symbol;
+    if (board[6] === board[7] && board[7] === board[8] && board[8] === symbol) return symbol;
+
+
+    if (board[0] === board[3] && board[3] === board[6] && board[6] === symbol) return symbol;
+    if (board[1] === board[4] && board[4] === board[7] && board[7] === symbol) return symbol;
+    if (board[2] === board[5] && board[5] === board[8] && board[8] === symbol) return symbol;
+
+
+    if (board[0] === board[4] && board[4] === board[8] && board[4] === symbol) return symbol;
+    if (board[2] === board[4] && board[4] === board[6] && board[4] === symbol) return symbol;
+
+    return '';
+}
 
 function Grid(props) {
     const [turn, setTurn] = useState(true)
     const [board, setBoard] = useState(Array(props.numbersOfCards).fill(''))
+    const [winnner, setWinner] = useState('')
 
     function play(index) {
         console.log("clicked", index);
+        if (turn === true) {
+            board[index] = '0'
+        } else {
+            board[index] = 'X'
+        }
+
+        const win = isWinner(board, turn ? '0' : 'X');
+        if (win) {
+            setWinner(win)
+        }
+        setTurn([...board])
         setTurn(!turn)
+
     }
 
+       function reset() {
+           setBoard(Array(props.numbersOfCards).fill())
+           setWinner(null)
+       }
+
     return (
-        <>
+        <div className='grid-wrapper'>
+            {winnner && (
+                <>
+                    <h1 className='turn-highlight'>Winner is {winnner} </h1>
+                    <button className='reset' onClick={reset}>Reset Game</button>
+                </>
+            )}
+
             <h1 className='turn-highlight'>Current Turn: {(turn) ? '0' : 'X'}</h1>
             <div className='grid'>
-               {board.map((value, index) => {
-                   return <Card onPlay={play} key={index} index={index} player={value} iconName="" />
+                {board.map((value, index) => {
+                    return <Card onPlay={play} key={index} index={index} player={value} iconName="" />
                 })}
             </div>
-        </>
+        </div>
     )
 }
 
-export default Grid
+export default Grid;
